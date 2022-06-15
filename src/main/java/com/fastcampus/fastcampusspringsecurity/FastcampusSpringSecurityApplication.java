260c48979e117b78a8e5039ceda9b3d5127575cb
2yxxn <yunji383614@gmail.com>
2022-06-15 16:15:12 +0900

    Initialize
    기본 + .yml 파일 세팅

---
+++ src/main/java/com/fastcampus/fastcampusspringsecurity/FastcampusSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusspringsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastcampusSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastcampusSpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/fastcampusspringsecurity/FastcampusSpringSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.fastcampusspringsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastcampusSpringSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
