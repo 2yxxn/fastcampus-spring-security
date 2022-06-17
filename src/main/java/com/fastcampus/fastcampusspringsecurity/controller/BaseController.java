5cc512b88d4083de4bb5279e53e06899c33fe545
2yxxn <yunji383614@gmail.com>
2022-06-17 17:40:28 +0900

    Add BaseController
    Default Page 로 이동하는 Controller 작성

---
+++ src/main/java/com/fastcampus/fastcampusspringsecurity/controller/BaseController.java
@@ -0,0 +1,17 @@
+package com.fastcampus.fastcampusspringsecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BaseController {
+    @GetMapping
+    public String rootPage() {
+        return "index";
+    }
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "index";
+    }
+}
